d88f32421d6a5c0f82945233dd2c5fbeee4f8ff5
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-06-12 11:29:47 +0200

    Remove old tests, add CI action, add precommit task

--- parent_test.ts
+++
@@ -1,6 +0,0 @@
-import { RatchetTree } from "./src/ratchet-tree.ts";
-
-console.log("parent(0):", RatchetTree.parent(0), "(expected: 1)");
-console.log("parent(2):", RatchetTree.parent(2), "(expected: 3)");
-console.log("parent(4):", RatchetTree.parent(4), "(expected: 5)");
-console.log("parent(6):", RatchetTree.parent(6), "(expected: 5)");
